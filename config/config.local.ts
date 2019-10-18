3b04711bbd23fea266e9f3080b62f46ae9dd7553
WangJinzhong <andywang7259@163.com>
2019-10-18 23:41:57 +0800

    config prod

--- config/config.local.ts
+++ config/config.local.ts
@@ -34,15 +34,15 @@ export default appInfo => {
     agent: false,
     // 是否加载开发者工具 graphiql, 默认开启。路由同 router 字段。使用浏览器打开该可见。
     graphiql: true,
-    //是否设置默认的Query和Mutation, 默认关闭
+    // 是否设置默认的Query和Mutation, 默认关闭
     defaultEmptySchema: true,
     // graphQL 路由前的拦截器
-    onPreGraphQL: function* (_ctx) { },
+    *onPreGraphQL (_ctx) { },
     // 开发工具 graphiQL 路由前的拦截器，建议用于做权限操作(如只提供开发者使用)
-    onPreGraphiQL: function* (_ctx) { },
+    *onPreGraphiQL (_ctx) { },
   };
 
-  config.middleware = ['graphql'];
+  config.middleware = [ 'graphql' ];
 
   return config;
 };
