cc189244b888c500227c6f3f427f787c4bcc4ea1
WangJinzhong <andywang7259@163.com>
2019-10-21 18:38:05 +0800

    config js -> ts

---
+++ config/locale/zh-CN.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  Account: '账号',
+  Email: '邮箱',
+  Password: '密码',
+  Upload: '上传',
+  'Please input account': '请输入账号',
+  'Please input password': '请输入密码',
+  'Please login': '请登录后继续',
+  'Hello {0}! My name is {1}.': '你好 {0}! 我的名字叫 {1}。',
+};
